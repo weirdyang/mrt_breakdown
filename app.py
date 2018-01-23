1e1fb7a78c03355f06a23e075b9d8ef0978e2664
captmomo <wy4786@gmail.com>
2018-01-23 15:28:48 +0800

    fixed dropdown

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 master_df = pd.read_json('static/master.json', orient='records')
 jsonfile = open('static/master.json', mode='r', encoding='utf-8')
 master_json = json.load(jsonfile)
+genre_list = ['Action', 'Drama', 'Comedy', 'Animation', 'All']
 
 
 @app.route('/', methods=['GET'])
@@ -39,23 +40,16 @@ def ratings():
     """
     return render_template('ratings.html')
 
-
-@app.route('/rate_compare', methods=['GET'])
-def compare_rate():
-    xy_chart = pygal.XY(stroke=False, show_legend=False)
-    xy_chart.title = "Movie's RT score vs IMDB Rating"
-    xy_chart._x_title = "Rotten Tomatoes Score"
-    xy_chart._y_tile = "IMDB Rating"
-    for item in master_json:
-        xy_chart.add(item['Movie'], [{'value': (item['RT'],  float(str(item['IMDB'])[:4]))}])
-    chart = xy_chart.render_data_uri()
-    return render_template('comparison.html', chart=chart)
-
-@app.route('/pygal_generate/', methods=['POST'])
+@app.route('/pygal_generate/', methods=['GET'])
 def pygal_generate():
-    search_term = request.form.get("genre")
+    """
+    Route that generates pygal chart
+    Attribution: http://biobits.org/bokeh-flask.html
+    """
+    search_term = request.args.get("genre")
     print(search_term)
-    if search_term == 'all':
+    if (search_term == None) or (search_term=='All') :
+        search_term = 'All'
         results = master_df.to_json(orient='records')
     else:
         filtered = master_df[master_df['genre'].str.contains(
@@ -69,7 +63,7 @@ def pygal_generate():
     for item in json_data:
         xy_chart.add(item['Movie'], [{'value': (item['RT'],  float(str(item['IMDB'])[:4]))}])
     chart = xy_chart.render_data_uri()
-    return render_template('comparison.html', chart=chart)
+    return render_template('comparison.html', chart=chart, genres=genre_list, selected_genre=search_term)
 
 
 

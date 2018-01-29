f853a34ad976520ae01571e9400b90e499c703de
captmomo <wy4786@gmail.com>
2018-01-30 00:42:45 +0800

    added kickstarter filter

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ master_df = pd.read_json('static/master.json', orient='records')
 jsonfile = open('static/master.json', mode='r', encoding='utf-8')
 master_json = json.load(jsonfile)
 genre_list = ['Action', 'Drama', 'Comedy', 'Animation', 'All']
+kickstart_df = pd.read_csv('static/kickstart200.tsv', sep='\t', encoding='utf-8')
 
 
 @app.route('/', methods=['GET'])
@@ -18,7 +19,7 @@ def index():
     """
     search_term = request.args.get("genre")
     print(search_term)
-    if (search_term == None) or (search_term=='All') :
+    if (search_term == None) or (search_term=='All'):
         search_term = 'All'
         results = master_df.to_json(orient='records')
     else:
@@ -34,7 +35,7 @@ def index():
         xy_chart.add(item['Movie'], [{'value': (item['Production Budget'],  item['Worldwide Gross']) }])
     chart = xy_chart.render_data_uri()
     return render_template('index.html', chart=chart, genres=genre_list, selected_genre=search_term)
-  
+
 @app.route('/ratings', methods=['GET'])
 def ratings():
     """
@@ -43,7 +44,7 @@ def ratings():
     """
     search_term = request.args.get("genre")
     print(search_term)
-    if (search_term == None) or (search_term=='All') :
+    if (search_term is None) or (search_term=='All') :
         search_term = 'All'
         results = master_df.to_json(orient='records')
     else:
@@ -75,7 +76,7 @@ def comparison():
     """
     search_term = request.args.get("genre")
     print(search_term)
-    if (search_term == None) or (search_term=='All') :
+    if (search_term is None) or (search_term=='All') :
         search_term = 'All'
         results = master_df.to_json(orient='records')
     else:
@@ -106,31 +107,54 @@ def kickstarter():
     """
     routes to kickstarter charts
     """
-    df = pd.read_csv('static/kickstart200.tsv', sep='\t', encoding='utf-8')
-    categories = df.category.unique()
-    cat_count = df.category.value_counts()
-    values = list(zip(categories, cat_count))
+    cat = kickstart_df.category.unique()
+    cat_count = kickstart_df.category.value_counts()
+    values = list(zip(cat, cat_count))
     bar_chart  = pygal.Bar(legend_at_bottom=True, legend_at_bottom_columns = 4, truncate_legend=-1)
     bar_chart.title = 'Succesful Kickstart Projects grouped by category'
-    categories = df.category.unique()
-    cat_count = df.category.value_counts()
-    values = list(zip(categories, cat_count))
     for item in values:
         if item[1] < 5:
             continue
         bar_chart.add(item[0], item[1])
     bars = bar_chart.render_data_uri()
-
     xy_chart = pygal.XY(stroke=False, show_legend=False)
     xy_chart.title = "Kickstarter Goal vs Pledged (USD)"
     xy_chart.x_title = "Goal"
     xy_chart.y_title = "Pledged"
-    df['usd_goal'] = df['goal'] * df['fx_rate']
-    for index, item in df.iterrows():
+    kickstart_df['usd_goal'] = kickstart_df['goal'] * kickstart_df['fx_rate']
+    for index, item in kickstart_df.iterrows():
+        percentage = float(str((item['pledged'] / item['usd_goal']) * 100)[:4])
         xy_chart.add(item['name'], [
-            {'value': (item['usd_goal'],  item['pledged']), 'label': item['category'] + " percentage: {}%".format((item['pledged'] / item['usd_goal']) * 100)}])
+            {'value': (item['usd_goal'],  item['pledged']), 'label': item['category'] + " Percentage: {}%".format(percentage)}])
     scatter = xy_chart.render_data_uri()
     return render_template('kickstart.html', bars=bars, scatter=scatter)
+
+@app.route('/categories', methods=['GET'])
+def categories():
+    """
+    fiters scatter plot based on dropdown selection
+    """
+    search_term = request.args.get("category")
+    print(search_term)
+    cat = kickstart_df.category.unique().tolist()
+    cat.append('All')
+    if (search_term is None) or (search_term=='All'):
+        search_term = 'All'
+        results = kickstart_df
+    else:
+        results = kickstart_df[kickstart_df['category'].str.contains(
+            search_term, case=False)]
+    xy_chart = pygal.XY(stroke=False, show_legend=False)
+    xy_chart.title = "Kickstarter Goal vs Pledged (USD)"
+    xy_chart.x_title = "Goal"
+    xy_chart.y_title = "Pledged"
+    results['usd_goal'] = results['goal'] * results['fx_rate']
+    for index, item in results.iterrows():
+        percentage = float(str((item['pledged'] / item['usd_goal']) * 100)[:4])
+        xy_chart.add(item['name'], [
+            {'value': (item['usd_goal'],  item['pledged']), 'label': item['category'] + " Percentage: {}%".format(percentage)}])
+    scatter = xy_chart.render_data_uri()
+    return render_template('categories.html', categories=cat, selected_category=search_term, chart=scatter)
 
 if __name__ == '__main__':
     app.run(debug=True)

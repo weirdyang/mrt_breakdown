a2a9cdd9e04142d236986b15e755fb1f8b6a82aa
captmomo <wy4786@gmail.com>
2018-03-20 05:30:51 +0800

    test

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ jsonfile = open('static/master.json', mode='r', encoding='utf-8')
 master_json = json.load(jsonfile)
 genre_list = ['Action', 'Drama', 'Comedy', 'Animation', 'All']
 kickstart_df = pd.read_csv('static/kickstart200.tsv', sep='\t', encoding='utf-8')
+dashboard_df = pd.read_csv('static/dashboard.tsv', sep='\t', encoding='utf-8')
 
 
 @app.route('/', methods=['GET'])
@@ -61,7 +62,7 @@ def ratings():
     chart = xy_chart.render_data_uri()
     return render_template('ratings.html', chart=chart, genres=genre_list, selected_genre=search_term)
 
-@app.route('/birthcharts.html', methods=['GET'])
+@app.route('/birthcharts', methods=['GET'])
 def birth_charts():
     """
     ROute that maps to birth rate comparison charts
@@ -117,6 +118,7 @@ def kickstarter():
             continue
         bar_chart.add(item[0], item[1])
     bars = bar_chart.render_data_uri()
+    ##generate XY chart
     xy_chart = pygal.XY(stroke=False, show_legend=False)
     xy_chart.title = "Kickstarter Goal vs Pledged (USD)"
     xy_chart.x_title = "Goal"
@@ -128,6 +130,59 @@ def kickstarter():
             {'value': (item['usd_goal'],  item['pledged']), 'label': item['category'] + " Percentage: {}%".format(percentage)}])
     scatter = xy_chart.render_data_uri()
     return render_template('kickstart.html', bars=bars, scatter=scatter)
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    """
+    Generates dashboard for week selected
+    """
+    weeks = dashboard_df['week'].tolist()
+    weeks = list(set(weeks))
+    search_term = request.args.get("week")
+    print(search_term)
+    if search_term is None:
+        search_term = weeks[-1]
+        results = dashboard_df[dashboard_df['week'] == search_term]
+    else:
+        results = dashboard_df[dashboard_df['week'] == search_term]
+    total_points = results['points'].sum()
+    total_sleep = results['sleep'].sum()
+    total_drive = results['driving'].sum()
+    results['timestamp'] = pd.to_datetime(results['date'], dayfirst=True)
+    #build sleep chart
+    sleep_line = pygal.DateTimeLine(x_label_rotation=0, show_legend=False, truncate_label=-1)
+    sleep_line.title = "Sleep Chart"
+    sleep_line.x_title = 'Dates'
+    sleep_line.y_title = 'Hours of Sleep'
+    sleep_values = list(zip(results['timestamp'].tolist(),results['sleep'].tolist()))
+    sleep_line.add('sleep line', sleep_values)
+    sleep_line.x_labels = results['timestamp'].tolist()
+    sleep_chart = sleep_line.render_data_uri()
+    #build drive chart
+    drive_bar = pygal.Bar(show_legend=False)
+    drive_bar.title = 'Driving hours logged'
+    drive_bar.x_title = 'Hours'
+    drive_bar.y_title = 'Day'
+    drive_values = results['driving'].tolist()
+    week_days = results['week_day'].unique()
+    values = list(zip(week_days, drive_values))
+    for item in values:
+        drive_bar.add(item[0], item[1])
+    drive_chart = drive_bar.render_data_uri()
+    #build points chart
+    points_pie = pygal.Pie(show_legend=False, inner_radius=.4)
+    results['points_percent'] = 100*results['points']/total_points
+    results['points_percent'] = results['points_percent'].round(2)
+    points_pie.title = 'Points distribution'
+    pie_values = list(zip(results['week_day'], results['points_percent']))
+    for item in pie_values:
+        points_pie.add(item[0], item[1])
+    chart = points_pie.render_data_uri()
+    return render_template('dashboard.html', weeks=weeks, selected_week=search_term, 
+                            chart=chart, sleep_chart=sleep_chart, drive_chart=drive_chart,
+                            total_drive=total_drive, total_points=total_points, total_sleep=total_sleep)
+
+
 
 @app.route('/categories', methods=['GET'])
 def categories():
